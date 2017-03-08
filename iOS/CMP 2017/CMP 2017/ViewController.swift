2f7401ddea1c36f40338e1597d0b6cbda0cb8fd4
Valentin Granados <valefcfhc@gmail.com>
2017-03-08 12:30:50 -0600

    first commit creo

---
+++ iOS/CMP 2017/CMP 2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CMP 2017
+//
+//  Created by Emmanuel Valentín Granados López on 08/03/17.
+//  Copyright © 2017 devworms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

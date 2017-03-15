64bd1afbd7258466e2846a50e7f70c916a52d089
salva100 <salva_bomba@hotmail.com>
2017-03-14 20:13:30 -0600

    Vistas 7,7.1,8,8.1,9,9.1,10,10.1,11,11.1,12
    Api de login y de registro implementado

--- Android/CMP2017/app/src/main/java/com/cmp2017/devworms/cmp2017/ExpositoresFrgament.java
+++ Android/CMP2017/app/src/main/java/com/cmp2017/devworms/cmp2017/ExpositoresFrgament.java
@@ -6,18 +6,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ListadoFragment extends Fragment {
+public class ExpositoresFrgament extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_listado, container, false);
+        View view = inflater.inflate(R.layout.fragment_expo, container, false);
 
-      String nombre = getArguments() != null ? getArguments().getString("nombre"):"Resultados";
-        TextView txtTitulo= (TextView) view.findViewById(R.id.txtTituloListado);
+
+        String nombre = getArguments() != null ? getArguments().getString("nombre"):"Expositores";
+        TextView txtTitulo= (TextView) view.findViewById(R.id.txtDescrip);
         txtTitulo.setText(nombre);
+
         return view;
 
 

ba0021efc60d11a769316c99854f081d92930542
salva100 <salva_bomba@hotmail.com>
2017-04-11 00:14:19 -0500

    aprendiendo coreData

--- iOS/CMP 2017/CMP 2017/DetalleViewController.swift
+++ iOS/CMP 2017/CMP 2017/DetalleViewController.swift
@@ -32,6 +32,7 @@ class DetalleViewController: UIViewController {
     var seccion = 0
     var detalle = [String: Any]()
     var imgData: Any?
+      var misExpositores = [String: Any]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +53,7 @@ class DetalleViewController: UIViewController {
             self.recomendaciones.text = detalle["recomendaciones"] as! String?
             
         case 2:
+        
             btn2.imageView?.image = #imageLiteral(resourceName: "04Agregar_a_mis_expositores")
             
             self.lbl2.text = "Contacto"
@@ -86,7 +88,28 @@ class DetalleViewController: UIViewController {
     }
     
     @IBAction func btnDos(_ sender: Any) {
+        if  self.seccion == 2 {
+        print("hola presion")
+             CoreDataHelper.saveData(data: detalle["id"] as! Int16, entityName: "MisExpositores", keyName: "idExpo")
+             CoreDataHelper.saveData(data: detalle["nombre"] as! String, entityName: "MisExpositores", keyName: "nombreExpo")
+             CoreDataHelper.saveData(data: detalle["url"] as! String, entityName: "MisExpositores", keyName: "urlExpo")
+             CoreDataHelper.saveData(data: detalle["telefono"] as! String, entityName: "MisExpositores", keyName: "telefonoExpo")
+             CoreDataHelper.saveData(data: detalle["email"] as! String, entityName: "MisExpositores", keyName: "emailExpo")
+             CoreDataHelper.saveData(data: detalle["acerca"] as! String, entityName: "MisExpositores", keyName: "acercaExpo")
+            
+            
+           let misExpositores = CoreDataHelper.fetchItem(entityName: "MisExpositores", keyName: "nombreExpo")!
+            for item in misExpositores{
+                
+                print ("Datos: \(item)" )
+                
+            }
+           
+        }
+        
     }
+    
+    
     
     @IBAction func btnTres(_ sender: Any) {
     }

b5fb7df994ac76b0bee8639e72d81ac393ef88d1
AndrewAlanGM <andrew-alan@hotmail.com>
2017-05-11 18:01:29 -0500

    Fix wait dialog mapa resuelto

--- Android/CMP2017/app/src/main/java/com/cmp2017/devworms/cmp2017/ClimaFragment.java
+++ Android/CMP2017/app/src/main/java/com/cmp2017/devworms/cmp2017/ClimaFragment.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,34 +20,47 @@ public class ClimaFragment extends Fragment
 {
     private ProgressDialog pDialog;
     String url, tipo;
+    private boolean isRedirected ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //inicializamos dialog
+        pDialog = new ProgressDialog(getActivity());
+        pDialog.setMessage("Cargando mapa" );
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+
         View view = inflater.inflate(R.layout.fragment_clima, container, false);
         WebView web = (WebView) view.findViewById(R.id.webViewClima);
         web.getSettings().setJavaScriptEnabled(true);
+
         web.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                if (!isRedirected) {
+                    super.onPageStarted(view, url, favicon);
+                    pDialog.show();
+                }
+
+                isRedirected = false;
+            }
+
 
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon){
-                super.onPageStarted(view, url, favicon);
-                pDialog = new ProgressDialog(getActivity());
-                pDialog.setMessage("Cargando mapa");
-                pDialog.setIndeterminate(false);
-                pDialog.setCancelable(false);
-                pDialog.show();
-
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                isRedirected = true;
+                return true;
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                log.d("url Termino","termino");
-                pDialog.dismiss();
-
-
+                if (!isRedirected) {
+                    super.onPageFinished(view, url);
+                    pDialog.dismiss();
+                }
             }
-
         });
+
         url =  getArguments().getString("url");
         tipo = getArguments().getString("tipo");
         log.d("url presentacion",url);
